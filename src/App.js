97480a173d478a944ecd1642f9f292c6c079b32a
magiccwarss <matem.mayreni@gmail.com>
2023-03-28 09:55:30 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './reset.css';
 import cross from './cross.svg';
-
+import crossWhite from './crossWhite.svg';
+import moon from './moon.svg';
+import moonWhite from './moonWhite.svg';
 function App() {
   const [newItem, setNewItem] = useState('');
   const [items, setItems] = useState([]);
+  const [darkmode, setDarkMode] = useState(false);
+  const [taskDate, setTaskDate] = useState('');
+
+  const handleTaskDateChange = (event) => {
+    setTaskDate(event.target.value);
+  };
 
   const addItem = () => {
     if (!newItem) {
@@ -16,11 +24,14 @@ function App() {
     const item = {
       id: Math.floor(Math.random() * 1000),
       value: newItem,
+      date: taskDate,
     };
 
     setItems((oldList) => [...oldList, item]);
 
     setNewItem('');
+
+    setTaskDate('');
   };
 
   function deleteItem(id) {
@@ -28,18 +39,49 @@ function App() {
     setItems(newArray);
   }
 
+  const handleSortTasks = () => {
+    const sortedTasks = [...items].sort((a, b) => new Date(a.date) - new Date(b.date));
+    setItems(sortedTasks);
+  };
+
+  function clearAllTodos() {
+    setItems([]);
+  }
+
   return (
     <div className="App">
-      <div className="toDo">
-        <div className="container">
-          <div className="toDo_content">
-            <div className="toDo_input">
+      <div className={darkmode ? 'toDo dark' : 'toDo'}>
+        <div className={darkmode ? 'container dark' : 'container'}>
+          <div className="text_and_moon">
+            <h1 className={darkmode ? 'general_text dark' : 'general_text'}>ToDo App</h1>
+            <div onClick={() => setDarkMode(!darkmode)} className="moon">
+              <img src={darkmode ? moonWhite : moon} alt="" />
+            </div>
+          </div>
+          <div className={darkmode ? 'toDo_content dark' : 'toDo_content'}>
+            <div className="toDoButtons">
+              <p className={darkmode ? 'you_have dark' : 'you_have'}>
+                You have{' '}
+                <span>
+                  {items.length} {items.length > 1 ? 'tasks' : 'task'}
+                </span>{' '}
+              </p>
+              <button className={darkmode ? 'clearAll dark' : 'clearAll'} onClick={clearAllTodos}>
+                Clear All Task
+              </button>
+              <button className={darkmode ? 'sortData dark' : 'sortData'} onClick={handleSortTasks}>
+                Sort By Date
+              </button>
+            </div>
+
+            <div className={darkmode ? 'toDo_input dark' : 'toDo_input'}>
               <input
                 onChange={(e) => setNewItem(e.target.value)}
                 type="text"
                 value={newItem}
                 placeholder="Type your task"
               />
+              <input type="date" value={taskDate} onChange={handleTaskDateChange} />
               <button className="toDo_btn" onClick={() => addItem()}>
                 Add Item
               </button>
@@ -47,18 +89,16 @@ function App() {
             <ul className="toDo_items">
               {items.map((item) => {
                 return (
-                  <li className="toDo_item" key={item.id}>
-                    {item.value}{' '}
+                  <li className={darkmode ? 'toDo_item dark' : 'toDo_item'} key={item.id}>
+                    <p>{item.value}</p>
+                    <p className="date">{item.date}</p>
                     <button className="toDo_item_btn" onClick={() => deleteItem(item.id)}>
-                      <img src={cross} alt="cross" />
+                      <img src={darkmode ? crossWhite : cross} alt="cross" />
                     </button>
                   </li>
                 );
               })}
             </ul>
-            <p className={items.length <= 0 ? 'you_have_zero' : 'you_have'}>
-              You have {items.length}
-            </p>
           </div>
         </div>
       </div>
